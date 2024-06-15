958fdf970939e6faec8b5cc553a9a7a789c83074
Deepanjali913 <deepanjaliaggarwal688@gmail.com>
2024-06-15 21:23:56 +0530

    g

---
+++ client/src/pages/CommunityPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function CommunityPage() {
+  return (
+    <div className='heading m-4 row '>
+        <div className='col-3'></div>
+        <div className='col col-6 text-center'>
+        <div className='card border border-bold border-rounded p-5'>
+            <span className='sub-heading'>"bjsdcbhbejnzbnxbsjehbzbcmnnjwhebmzhbc  dcbseubkbdjcbiejcbsmdchsbm"</span>
+
+        </div>
+        </div>
+    </div>
+  )
+}
