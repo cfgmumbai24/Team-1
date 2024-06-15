183c8232b998c728c8aeb93f13cd89085066b403
Salabh Rai <salabh.rai@gmail.com>
2024-06-15 23:57:16 +0530

    Fixed Ui of Home page

--- client/src/pages/VideoPage.jsx
+++ client/src/pages/VideoPage.jsx
@@ -4,15 +4,20 @@ const VideoPage = () => {
   const videos = [
     { src: 'https://www.youtube.com/embed/IbjMvJnx_GM', title: 'Video 1' },
     { src: 'https://www.youtube.com/embed/6j1Oj5N7kwE', title: 'Video 2' },
+    {
+      src: 'https://www.youtube.com/watch?v=f2EqECiTBL8&t=22786s', title: 'Video 3'
+    },
+    {src: 'https://www.youtube.com/watch?v=LMT15LKWE8Q', title: 'Video 4'}
     // Add more videos as needed
   ];
 
+
   return (
-    <div>
+    <div className="video-row">
       {videos.map((video, index) => (
-        <div key={index} className="card">
+        <div key={index} className="card " >
           <iframe 
-            width="560" 
+            width="320" 
             height="315" 
             src={video.src} 
             title={video.title}
