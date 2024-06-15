9ae07aaee25fd50379d8262858e06545f5792831
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-06-16 00:44:17 +0530

    test

--- client/src/pages/CommunityPage.jsx
+++ client/src/pages/CommunityPage.jsx
@@ -1,15 +1,15 @@
-//import React from 'react'
+import React from 'react'
+import './community.css'
+import ForumCard from '../components/ForumCard'
 
 export default function CommunityPage() {
   return (
-    <div className='heading m-4 row '>
-        <div className='col-3'></div>
-        <div className='col col-6 text-center'>
-        <div className='card border border-bold border-rounded p-5'>
-            <span className='sub-heading'>&quot;bjsdcbhbejnzbnxbsjehbzbcmnnjwhebmzhbc  dcbseubkbdjcbiejcbsmdchsbm&quot;</span>
-
-        </div>
-        </div>
+    <div className='container'>
+      <div className='parent'>
+        <ForumCard />
+        <ForumCard />
+        <ForumCard />
+      </div>
     </div>
   )
 }
