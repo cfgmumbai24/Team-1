9af80bbfd391baa36e842e510d8866c063b6a82e
Tripti Mahajan <triptimahajan3@gmail.com>
2024-06-15 20:49:58 +0530

    function created

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -48,6 +48,62 @@ export const getCommentsByPostId = async (req, res) => {
     }
 };
 
+export const getCommentsByCommentId = async (req, res) => {
+    const { id } = req.params; // Assuming 'id' is the parameter name for comment_id
+
+    try {
+        const comments = await Comment.find({ comment_id: id });
+
+        if (comments.length === 0) {
+            return res.status(404).json({ error: 'No comments found with the provided id' });
+        }
+
+        res.status(200).json(comments);
+    } catch (error) {
+        console.error("Error retrieving comments by comment_id:", error.message);
+        res.status(500).json({ error: 'Failed to retrieve comments by comment_id' });
+    }
+};
 
 
+export const incrementUpvotes = async (req, res) => {
+    const { comment_id } = req.params;
 
+    try {
+        const comment = await Comment.findOneAndUpdate(
+            { _id: comment_id },
+            { $inc: { no_of_upvotes: 1 } }, 
+            { new: true } 
+        );
+
+        if (!comment) {
+            return res.status(404).json({ error: 'Comment not found' });
+        }
+
+        res.status(200).json(comment);
+    } catch (error) {
+        console.error("Error incrementing upvotes:", error.message);
+        res.status(500).json({ error: 'Failed to increment upvotes' });
+    }
+};
+
+export const decrementDownvotes = async (req, res) => {
+    const { comment_id } = req.params;
+
+    try {
+        const comment = await Comment.findOneAndUpdate(
+            { _id: comment_id },
+            { $inc: { no_of_downvotes: 1 } }, // Decrementing the no_of_downvotes field by 1
+            { new: true } // To return the updated document
+        );
+
+        if (!comment) {
+            return res.status(404).json({ error: 'Comment not found' });
+        }
+
+        res.status(200).json(comment);
+    } catch (error) {
+        console.error("Error decrementing downvotes:", error.message);
+        res.status(500).json({ error: 'Failed to decrement downvotes' });
+    }
+};
