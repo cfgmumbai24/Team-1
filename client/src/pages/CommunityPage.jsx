0909c401db28d1af46dfe388af81afecb6f4c82c
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-06-16 02:20:02 +0530

    backend + frontend

--- client/src/pages/CommunityPage.jsx
+++ client/src/pages/CommunityPage.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './community.css'
 import ForumCard from '../components/ForumCard'
+import { useLanguage } from '../contexts/languageContext'
+import useGetForumPosts from '../hooks/forum/useGetForumPosts';
 
 export default function CommunityPage() {
+   const {isEnglish, toggleLanguage} = useLanguage();
+   console.log("is eng: ",isEnglish)
+   const english = localStorage.getItem(isEnglish)
+
+   const { posts, isGettingPosts} = useGetForumPosts(isEnglish);
+
+   console.log("posts: ", posts)
+
+   useEffect(() => {
+
+   }, []) 
   return (
     <div className='container'>
       <div className='parent'>
-        <ForumCard />
-        <ForumCard />
-        <ForumCard />
+        {posts?.map((item, index) => {
+          return <ForumCard key={index} />
+        })}
       </div>
     </div>
   )
